5dc9207c091dfab6fa627c1314210922dbbcf606
onat <onatbudak@gmail.com>
2025-05-02 21:55:03 +0300

    tahta

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -8,41 +8,41 @@ const Game = () => {
             <div className='w-40rem h-36rem bg-red-300 flex'>
 
                     <div className='w-6 bg-red-400 mr-1'>
-                        <div className='flex bg-green-100 h-18rem'>
-                            <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mt-0'></div>
+                        <div className='flex bg-green-100 h-18rem '>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
                         </div>
                         <div className='flex bg-green-100 h-18rem'>
-                            <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                            <div className='w-2 bg-red-500 m-1 mb-0'></div>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
                         </div>
                     </div>
               
                 <div className='w-6 bg-red-400 ml-1'>
-                    <div className='flex bg-green-100 h-18rem'>
-                        <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mt-0'></div>
-                    </div>
-                    <div className='flex bg-green-100 h-18rem'>
-                        <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                        <div className='w-2 bg-red-500 m-1 mb-0'></div>
-                    </div>
+                    <div className='flex bg-green-100 h-18rem '>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
+                            <div className='triangle-top m-1 '></div>
+                        </div>
+                        <div className='flex bg-green-100 h-18rem'>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
+                            <div className='triangle-bottom m-1 '></div>
+                        </div>
                 </div>
             </div>
             

17912e4a2ee5a22420faf47263a0bb31c04ac538
aliemre2023 <aliemrekaya239@gmail.com>
2025-05-02 23:17:58 +0300

    dice temp

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -5,6 +5,16 @@ const Game = () => {
 
     return (
         <div className='flex w-12 h-screen justify-content-center align-items-center bg-blue-300'>
+            <div className='w-12'>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                    <div>6</div>
+                </div>
+            </div> 
             <div className='w-40rem h-36rem bg-red-300 flex'>
 
                     <div className='w-6 bg-red-400 mr-1'>
@@ -43,6 +53,25 @@ const Game = () => {
                             <div className='triangle-bottom m-1 '></div>
                             <div className='triangle-bottom m-1 '></div>
                         </div>
+                </div>
+            </div>
+
+            <div className='w-12 flex align-items-center justify-content-center'>
+                <div className='bg-red-700 m-4'>
+                    <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
+                </div>
+                <div className='bg-red-700 m-4'>
+                <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
+                    <div className='p-1'>1</div>
                 </div>
             </div>
             
